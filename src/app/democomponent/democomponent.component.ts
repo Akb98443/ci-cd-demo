2af82a3863985ad34aabb4daa0f02adda1c999c9
Akb998877 <AVANISH-KUMAR.BIND@CAPGEMINI.COM>
2025-07-31 16:04:11 +0530

    added a new component

---
+++ src/app/democomponent/democomponent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-democomponent',
+  templateUrl: './democomponent.component.html',
+  styleUrls: ['./democomponent.component.css']
+})
+export class DemocomponentComponent {
+
+}
