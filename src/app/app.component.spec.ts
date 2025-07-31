183019969c6540ee40b4c90639df67fbd68419f2
Akb998877 <AVANISH-KUMAR.BIND@CAPGEMINI.COM>
2025-07-31 16:28:54 +0530

    added democomponent in the app.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { DemocomponentComponent } from './democomponent/democomponent.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    declarations: [AppComponent , DemocomponentComponent]
   }));
 
   it('should create the app', () => {
